53b0dab41701c177864f07ff30e3cfc28052ee44
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-21 22:19:35 +0530

    Add: To get the coupon numbers

--- src/main/java/com/logicalprograms/LogicalPrograms.java
+++ src/main/java/com/logicalprograms/LogicalPrograms.java
@@ -1,9 +1,26 @@
 package com.logicalprograms;
+import java.security.SecureRandom;
+import java.util.*;
 
 public class LogicalPrograms 
 {
+
+    static void randomGenerator(int number) {
+		SecureRandom r= new SecureRandom();//This class provide cryptographically strong random number generator (RNG). 
+		int i=1;
+		while(i<=number) {
+			int Coupon=r.nextInt(number*number);
+			System.out.println(Coupon);
+			i++;
+		}
+	}
     public static void main( String[] args )
     {
         System.out.println( "Logical Programs" );
+
+        System.out.println("Enter The No. Of Coupon Numbers You Want:");
+		Scanner input =new Scanner(System.in);
+		int number=input.nextInt();
+		randomGenerator(number);
     }
 }
