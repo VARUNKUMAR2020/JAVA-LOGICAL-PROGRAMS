eee8b91bc5534e0fff17a267a7317e1209a17848
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-21 22:07:40 +0530

    Add: Fibonacci series of a number

--- src/main/java/com/logicalprograms/LogicalPrograms.java
+++ src/main/java/com/logicalprograms/LogicalPrograms.java
@@ -1,9 +1,28 @@
 package com.logicalprograms;
 
-public class LogicalPrograms 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Logical Programs" );
+import java.util.*;;
+
+public class LogicalPrograms {
+    public static void main(String[] args) {
+        System.out.println("Logical Programs");
+        System.out.println("Fibonacci series");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int number = input.nextInt();
+        int a = 0, b = 1, c = 0;
+        System.out.print("First " + number + " terms: ");
+
+        for (int integer = 1; integer <= number; integer++) {
+            if (integer == 1)
+                System.out.println(a);
+            if (integer == 2)
+                System.out.println(b);
+            if (integer > 2) {
+                c = a + b; // Next term is generated here.
+                System.out.println(c);
+                a = b; // Changing the value of existing integer to the next integer.
+                b = c; // Changing the value of existing integer to the next integer.
+            }
+        }
     }
 }
